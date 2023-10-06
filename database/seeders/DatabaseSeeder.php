d26b6a83643548d834957f7f5d8b1eee53acd6c8
AslanAV <autlevaslsn@yandex.ru>
2023-10-06 10:42:35 +0300

    add breeze authentication, add admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,10 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'admin',
+            'email' => env('ADMIN_EMAIL', 'admin@admin.ru'),
+            'password' => env('ADMIN_PASSWORD', 'adminadmin')
+        ]);
     }
 }
